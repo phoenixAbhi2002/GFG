e9296e5a4ae0b8b953dbe44ef06a134a1d0f484a
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2022-10-22 14:48:09 +0530

    Added solution - LeetHub

--- Filling Bucket - GFG/filling-bucket.cpp
+++ Filling Bucket - GFG/filling-bucket.cpp
@@ -5,42 +5,22 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int mod=1e8;
-    
-    /*int solve(int N, vector<int>dp){
-        
-        //base cases
-        if(N==0)
-            return 1;
-        if(N<0)
-            return 0;
-            
-        if(dp[N]!=-1)
-            return dp[N];
-            
-        int ansFrom1=0, ansFrom2=0;
-        
-        //in case of chossing 1
-        ansFrom1+=solve(N-1, dp);
-        
-        //in case of chossing 2
-        ansFrom2+=solve(N-2, dp);
-        
-        return dp[N] = (ansFrom1 + ansFrom2)%mod;
-    }*/
-    
     int fillingBucket(int N) {
         // code here
         
-        vector<int>dp(N+1, 0);
+        int mod=1e8;
         
-        dp[0]=1;
+        if(N<=3)
+            return N;
+            
+        int prev1=1, prev2=2;
         
-        for(int i=1;i<=N;i++){
-            dp[i] = (dp[i-1] + dp[i-2])%mod;
+        for(int i=1;i<=N-2;i++){
+            int curr=(prev1+prev2)%mod;
+            prev1=prev2;
+            prev2=curr;
         }
-        
-        return dp[N];
+        return prev2;
     }
 };
 
