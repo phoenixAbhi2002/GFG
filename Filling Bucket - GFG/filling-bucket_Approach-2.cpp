d7087f5932df7dc3e8281bbc4c02644d06b41f5d
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2022-10-22 14:50:25 +0530

    Create filling-bucket_Approach-2.cpp

---
+++ Filling Bucket - GFG/filling-bucket_Approach-2.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int fillingBucket(int N) {
+        // https://www.youtube.com/watch?v=-XOxbgCejtg&t=1065s
+        
+        int mod=1e8;
+        
+        if(N<=3)
+            return N;
+            
+        int prev1=1, prev2=2;
+        
+        for(int i=1;i<=N-2;i++){
+            int curr=(prev1+prev2)%mod;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        
+        cin>>N;
+
+        Solution ob;
+        cout << ob.fillingBucket(N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
