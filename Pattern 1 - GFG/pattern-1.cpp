5415c14f26bd659f94c293e7960679df71b3f7ba
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2022-11-05 19:36:13 +0530

    Added solution - LeetHub

--- Pattern 1 - GFG/pattern-1.cpp
+++ Pattern 1 - GFG/pattern-1.cpp
@@ -7,7 +7,7 @@ using namespace std;
 class Solution {
   public:
     void printSquare(int n) {
-        // code here
+        
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 cout<<"*"<<" ";
