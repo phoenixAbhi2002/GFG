a0807eab0abefdb3ed484cf793eda3f1069cfe26
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2022-11-12 01:30:13 +0530

    using STL functions

---
+++ Permutations of a given string - GFG/approach2.cpp
@@ -0,0 +1,9 @@
+vector<string>find_permutation(string S){
+    sort(S.begin(), S.end());
+	  vector<string>ans;
+	  ans.push_back(S);
+	  while(next_permutation(S.begin(),S.end())){
+		  ans.push_back(S);
+	  }
+	  return ans;
+}
