943355dd3e4e15811623d61c6f2228a53156098b
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2022-11-13 12:11:00 +0530

    Added solution - LeetHub

---
+++ Rightmost different bit - GFG/rightmost-different-bit.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution
+{
+    public:
+    //Function to find the first position with different bits.
+    int posOfRightMostDiffBit(int m, int n)
+    {
+        // Your code here
+        int change = 1;
+        while(n>0 || m>0) {
+            if(n%2 != m%2) {
+               return change;
+            }
+            change++;
+            n = n/2; 
+            m = m/2;
+        }
+        return -1;
+    }
+};
+
+//{ Driver Code Starts.
+
+// Driver Code
+int main()
+{   
+    int t;
+    cin>>t; //input number of testcases
+    while(t--)
+    {
+         int m,n;
+         cin>>m>>n; //input m and n
+         Solution ob;
+         cout << ob.posOfRightMostDiffBit(m, n)<<endl;
+    }
+    return 0;     
+} 
+// } Driver Code Ends
