4cc565341ff5287eddcba9c79a3bc4ac3983bf5b
Abhijeet Sahoo <92420934+phoenixAbhi2002@users.noreply.github.com>
2023-09-21 23:12:36 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,70 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+double MedianOfArrays(vector<int>& array1, vector<int>& array2);
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    public:
+    double MedianOfArrays(vector<int>& nums1, vector<int>& nums2)
+    {
+        // Your code goes here
+        int n1=nums1.size();
+        int n2=nums2.size();
+        int i=0;
+        int j=0;
+        int lastindex=-1;
+             
+        vector<int>v(n1+n2,0);
+        
+        while(i<n1&&j<n2)
+        {
+            if(nums1[i]<=nums2[j])
+                v[++lastindex]=nums1[i++];
+            else
+                v[++lastindex]=nums2[j++];
+        }
+        
+        while(i<n1)
+            v[++lastindex]=nums1[i++];
+        while(j<n2)
+            v[++lastindex]=nums2[j++];
+        
+        int n=n1+n2;
+        return n%2?v[n/2]:(v[n/2]+v[n/2-1])/2.0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int m,n;
+        cin>>m;
+        vector<int> array1(m);
+        for (int i = 0; i < m; ++i){
+            cin>>array1[i];
+        }
+        cin>>n;
+        vector<int> array2(n);
+        for (int i = 0; i < n; ++i){
+            cin>>array2[i];
+        }
+        Solution ob;
+        cout<<ob.MedianOfArrays(array1, array2)<<endl;
+    }
+    return 0; 
+}
+
+
+
+// } Driver Code Ends
